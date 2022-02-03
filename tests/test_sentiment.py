2bcb12672fba40cb27f85372863412ce5f3baac1
Piotr Krawiec <piotr.krawiec23@gmail.com>
2022-02-03 10:19:30 +0100

    Add tests

---
+++ tests/test_sentiment.py
@@ -0,0 +1,21 @@
+from fastapi.testclient import TestClient
+
+from src.main import app
+
+client = TestClient(app)
+
+
+def test_health():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {"API": "online"}
+
+
+def test_sentiment_prediction():
+    response = client.post(
+        "/sentiment",
+        json={"value": "Kill yourself"}
+    )
+    assert response.status_code == 200
+    assert response.json()
+    assert response.json()["label"] == "negative"
