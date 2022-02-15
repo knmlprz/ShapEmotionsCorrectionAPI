34ecabc7ac9cd440e5494ad6bd26df6ddc97f175
Piotr Krawiec <piotr.krawiec23@gmail.com>
2022-02-15 21:52:04 +0100

    Add api limiter

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 import os
 import torch
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
+
 from fastapi.middleware.cors import CORSMiddleware
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
+from slowapi.util import get_remote_address
 
 from src.models.sentiment import (
     Sentiment,
@@ -19,8 +23,11 @@ COMPUTE_DEVICE = int(os.getenv("COMPUTE_DEVICE"))
 torch.set_grad_enabled(False)
 torch.set_num_threads(1)
 
+limiter = Limiter(key_func=get_remote_address)
 # Create app
 app = FastAPI()
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 # Allow CORS
 origins = ["*"]
@@ -42,15 +49,18 @@ class Text(BaseModel):
 
 
 @app.get("/health")
-def read_root():
+@limiter.limit("5/minute")
+def read_root(request: Request):
     return {"API": "online"}
 
 
 @app.post("/sentiment", response_model=SentimentPredictResponse)
-def get_sentiment(text: Text):
+@limiter.limit("10/minute")
+def get_sentiment(text: Text, request: Request):
     return sentiment.predict(text.value)
 
 
 @app.post("/sentiment/explain", response_model=SentimentExplainResponse)
-def get_explain(text: Text):
+@limiter.limit("3/minute")
+def get_explain(text: Text, request: Request):
     return sentiment.explain(text.value)
